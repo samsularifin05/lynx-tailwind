c4e7732c7837bc3bee7c9727fe68ed39715ac86f
samsularifin05 <samsul026@gmail.com>
2025-03-19 15:53:36 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 export function App() {
   return (
-    <view className="flex items-center justify-center h-screen bg-red-700">
-      <text className="text-white">HALLO</text>
+    <view className="flex flex-col h-screen ">
+      <view className="flex items-center justify-center flex-1 h-screen text-black bg-red-700 b">
+        <text className="text-white">HALLO 1</text>
+      </view>
+      <view className="flex items-center justify-center flex-1 h-screen text-black bg-white">
+        <text>HALLO 2</text>
+      </view>
     </view>
   );
 }
